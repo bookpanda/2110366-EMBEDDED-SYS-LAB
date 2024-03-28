16038632b05fb081702eb45ed2bbe2a2f4f3c1d0
Idhibhat Pankam <marginpankam@gmail.com>
2024-03-28 23:50:45 +0700

    feat: lab4.3 new

--- lab4.cpp
+++ lab4.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(void) {
     //Lab 4.1 Timer increase brightness
-    //100ms timer: 0.1 = ( (1 + 1) * (399 + 1) )/8M
+    //100us timer: 0.0001 = ( (1 + 1) * (399 + 1) )/8M, PWM gen
     HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
     while (1)
     {
@@ -30,5 +30,27 @@ int main(void) {
         sprintf (buf, "%d\r\n" , ldrval);
         HAL_UART_Transmit(&huart2, buf, strlen(buf), 1000);
         HAL_Delay(100);
+    }
+
+    //Lab4.3 control LED with LDR
+    //create ADC1_IN0 and timer
+    HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
+    int adcval = 0;
+    char buf[256];
+    while (1)
+    {
+        HAL_ADC_Start(&hadc1);
+        if (HAL_ADC_PollForConversion(&hadc1, 1000) == HAL_OK) {
+            adcval = HAL_ADC_GetValue(&hadc1);
+            sprintf (buf, "%d\r\n" , adcval);
+            int c = 21 - adcval; //max - curr
+            int p = 21 - 1; //max - min
+            int brightness = (1 - (c/p)) * 100;
+            TIM4->CCR1 = brightness;
+            HAL_UART_Transmit(&huart2, buf, strlen(buf), 1000);
+        }
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
     }
 }
